f1c7a94666f0b600123b42af0e5d9b4b0b321d7a
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-09-05 18:30:19 -0700

    Added app items, migrations and .gitignore.

--- djangoByBuilding/core/urls.py
+++ djangoByBuilding/core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -6,4 +8,4 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contact", views.contact, name="contact")
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
